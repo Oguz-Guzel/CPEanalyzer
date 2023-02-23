9d4ffa796c5ba42315548106814eb2e7b90a521e
Oğuz Güzel <44399848+Oguz-Guzel@users.noreply.github.com>
2023-02-23 13:56:10 +0100

    Update SiStripHitResolFromCalibTree.cc

--- plugins/SiStripHitResolFromCalibTree.cc
+++ plugins/SiStripHitResolFromCalibTree.cc
@@ -223,13 +223,12 @@ void SiStripHitResolFromCalibTree::algoEndJob() {
 }
 
 void SiStripHitResolFromCalibTree::algoAnalyze(const edm::Event& e, const edm::EventSetup& c) {
-
+  edm::ESGetToken<TrackerTopology, TrackerTopologyRcd> token1_;
   edm::ESHandle<TrackerGeometry> tracker;
   c.get<TrackerDigiGeometryRecord>().get( tracker );
   const TrackerGeometry * tkgeom=&(* tracker);
   
   //Retrieve tracker topology from geometry
-  edm::ESGetToken<TrackerTopology, TrackerTopologyRcd> token1_;
   edm::ESHandle<TrackerTopology> tTopoHandle;
   c.get<TrackerTopologyRcd>().get(tTopoHandle);
   const TrackerTopology* const tTopo = tTopoHandle.product();
